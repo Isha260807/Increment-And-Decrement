c0047084c11dbac17e9c6a1c0f2976fe935ca838
Nisha Kumari <yadavnisha7720@gmail.com>
2025-08-31 12:38:50 +0530

    first commit

---
+++ vite-project/src/data.jsx
@@ -0,0 +1,39 @@
+const reviews  = [
+    {
+        id:1,
+        name:"Pranay Gupta",
+        job:"Software Engineer",
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text:"Detail-oriented and innovative Software Engineer with experience in designing, developing, and maintaining scalable applications. Skilled in full-stack development, problem-solving, and optimizing code performance."
+    },
+    {
+        id:2,
+        name:"Abir Pal",
+        job:"Graphic Designer",
+        image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text:"Detail-oriented and innovative Software Engineer with experience in designing, developing, and maintaining scalable applications. Skilled in full-stack development, problem-solving, and optimizing code performance."
+    },
+    {
+        id:3,
+        name:"Soumya Banerjee",
+        job:"Marketing Manager",
+        image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text:"Detail-oriented and innovative Software Engineer with experience in designing, developing, and maintaining scalable applications. Skilled in full-stack development, problem-solving, and optimizing code performance."
+    },
+    {
+        id:4,
+        name:"Saikat Mukherjee",
+        job:"Content Strategist",
+        image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text:"Detail-oriented and innovative Software Engineer with experience in designing, developing, and maintaining scalable applications. Skilled in full-stack development, problem-solving, and optimizing code performance."
+    },
+    {
+        id:1,
+        name:"Aritra Biswas",
+        job:"Data Analyst",
+        image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+        text:"Detail-oriented and innovative Software Engineer with experience in designing, developing, and maintaining scalable applications. Skilled in full-stack development, problem-solving, and optimizing code performance."
+    },
+];
+
+export default reviews;
